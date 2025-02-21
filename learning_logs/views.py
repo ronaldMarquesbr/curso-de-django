d9d6a450619afc9df52e85a2da212dcce62eff82
Ronald Marques <ronaldpmarques01@gmail.com>
2025-02-21 16:37:53 -0300

    Página de tópicos

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
+from .models import Topic
 
 
 def index(request):
     """Página principal do Learning_Log"""
     return render(request, 'learning_logs/index.html')
+
+
+def topics(request):
+    """Mostra todos os assuntos"""
+
+    topics_set = Topic.objects.order_by('date_added')
+    context = {'topics': topics_set}
+
+    return render(request, 'learning_logs/topics.html', context)
